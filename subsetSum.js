8cfa029ed65ea4c79a780cb05a573be09608c8fa
Lin Xia <linyianxia@hotmail.com>
2020-02-02 00:33:01 -0500

    think permutations w/ combos, except we will not recycle the head back into the combo, & rmmbr that [[]] is a meaningful empty unlike []

--- subsetSum.js
+++ subsetSum.js
@@ -6,24 +6,6 @@ determine if a sublist can sum to the target
 1 2 5 with t = 3 is true ;  t = 4 is false
 */
 
-function subset(list, target) {
-  //base case: lists mean trim to 0 length
-  if (!list.length) {
-    console.log("base case here");
-  }
-  //recursive case:
-  let head = list.pop(); // pick a num as the main base to start the combo tree
-  for (let i = 0; i < list.length; i++) {
-    //for adding head to one other el
-    if (head + list[i] === target) {
-      return true;
-    }
-  }
-
-  //default case, nothing triggers true:
-  return false;
-}
-
 /*
 each el should have combos of any len (max arr.len)
 factorial?
@@ -36,4 +18,51 @@ factorial?
 5: 5,
     5, 1
     5, 2
+
+What should my 'subset' look like?
+I want my 'head' to add with the 'next'
+the 'head' should exhaust all possible options of 'next' (list w/o 'head')
+before diving a branch deeper where
+the 'head' is now one of the 'next' (list will now have 2 'head' val removed)
+when we do extend a branch, it'll be the OG 'head' plus these new 2 values
+>>check if head + recursion(head, next) === target
+this will end when there is no more 'next'
 */
+
+function subset(list, target) {
+  if (!list.length) {
+    return [];
+  }
+  const result = [];
+  function helper(arr, head = []) {
+    if (!arr.length) {
+      return [[]];
+    }
+    if (!head.length) {
+      result.push([]);
+    }
+    for (let i = 0; i < arr.length; i++) {
+      let remaining = arr.slice(i + 1);
+      let oneCombo = head.concat([arr[i]]);
+      result.push(oneCombo);
+      helper(remaining, oneCombo);
+    }
+  }
+  helper(list);
+  // console.log(result);
+  const sums = result.map(combo => {
+    return combo.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+  });
+  // console.log(sums);
+  return sums.includes(target);
+}
+
+let test1 = [1, 2, 5];
+let listA = [0, 1, 2];
+let listB = [1, 2, 3, 4];
+console.log(subset(test1, 3)); //T
+console.log(subset(test1, 4)); //F
+console.log(subset(listA, 5)); //F
+console.log(subset(listB, 5)); //T
