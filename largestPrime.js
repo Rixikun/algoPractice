4da3b535171c4d67e408b45175ae73b1042166c7
Lin Xia <linyianxia@hotmail.com>
2020-02-03 22:32:25 -0500

    successfully recursiond the large num down to smaller nums

--- largestPrime.js
+++ largestPrime.js
@@ -6,45 +6,46 @@ too large, i dunno how to approach this for efficiency
 
 */
 
-function largestPrime(num){
-    console.log('pass', num)
-    if(isPrime(num)) {
-        return num
-    }
-    if(num % 2 === 0) {
-       return largestPrime(num/2)
-    }
-    if(!isPrime(num)) {
-        return largestPrime(isFactor(num))
-    }
+function largestPrime(num) {
+  //   console.log("pass", num);
+  if (isPrime(num)) {
+    return num;
+  }
+  if (num % 2 === 0) {
+    return largestPrime(num / 2);
+  }
+  if (!isPrime(num)) {
+    // console.log("divide me", num);
+    return largestPrime(num / isFactor(num));
+  }
 }
 
 function isFactor(num) {
-    if(!isPrime(num)){
-        for(let i = 3; i * i <= num; i++) {
-            if(num % i === 0){
-                return i
-            }
-        }
-    }
-}
-
-let isPrime = (n) => {
-    for (let i = 2; i * i <= n; i++) {
-      if (isPrime(i) && n % i === 0) {
-        return false;
+  if (!isPrime(num)) {
+    for (let i = 3; i * i <= num; i++) {
+      if (num % i === 0) {
+        return i;
       }
     }
-    return true;
-  };
+  }
+}
+
+let isPrime = n => {
+  for (let i = 2; i * i <= n; i++) {
+    if (isPrime(i) && n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 // function isPrime(n) {
 //     let count = 3;
 //     while(count < )
 // }
 
-// console.log(largestPrime(600851475143))
-console.log(largestPrime(7272))
+console.log(largestPrime(600851475143));
+// console.log(largestPrime(7272));
 // console.log(largestPrime(100))
 // console.log(isPrime(25))
 // console.log(isPrime(7272))
