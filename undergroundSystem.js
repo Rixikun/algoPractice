a6fd17e8a1ef3fc358eef61ff09899cf345caf8a
LinXia <linyianxia@hotmail.com>
2020-10-24 21:47:33 -0400

    undergroundSystem

---
+++ undergroundSystem.js
@@ -0,0 +1,104 @@
+/* 
+ Implement the class UndergroundSystem that supports three methods:
+
+1. checkIn(int id, string stationName, int t)
+
+A customer with id card equal to id, gets in the station stationName at time t.
+A customer can only be checked into one place at a time.
+2. checkOut(int id, string stationName, int t)
+
+A customer with id card equal to id, gets out from the station stationName at time t.
+3. getAverageTime(string startStation, string endStation) 
+
+Returns the average time to travel between the startStation and the endStation.
+The average time is computed from all the previous traveling from startStation to endStation that happened directly.
+Call to getAverageTime is always valid.
+You can assume all calls to checkIn and checkOut methods are consistent. That is, if a customer gets in at time t1 at some station, then it gets out at time t2 with t2 > t1. All events happen in chronological order.
+
+ 
+Example 1:
+Input
+["UndergroundSystem","checkIn","checkIn","checkIn","checkOut","checkOut","checkOut","getAverageTime","getAverageTime","checkIn","getAverageTime","checkOut","getAverageTime"]
+[[],[45,"Leyton",3],[32,"Paradise",8],[27,"Leyton",10],[45,"Waterloo",15],[27,"Waterloo",20],[32,"Cambridge",22],["Paradise","Cambridge"],["Leyton","Waterloo"],[10,"Leyton",24],["Leyton","Waterloo"],[10,"Waterloo",38],["Leyton","Waterloo"]]
+
+Output
+[null,null,null,null,null,null,null,14.00000,11.00000,null,11.00000,null,12.00000]
+
+Explanation
+UndergroundSystem undergroundSystem = new UndergroundSystem();
+undergroundSystem.checkIn(45, "Leyton", 3);
+undergroundSystem.checkIn(32, "Paradise", 8);
+undergroundSystem.checkIn(27, "Leyton", 10);
+undergroundSystem.checkOut(45, "Waterloo", 15);
+undergroundSystem.checkOut(27, "Waterloo", 20);
+undergroundSystem.checkOut(32, "Cambridge", 22);
+undergroundSystem.getAverageTime("Paradise", "Cambridge");       // return 14.00000. There was only one travel from "Paradise" (at time 8) to "Cambridge" (at time 22)
+undergroundSystem.getAverageTime("Leyton", "Waterloo");          // return 11.00000. There were two travels from "Leyton" to "Waterloo", a customer with id=45 from time=3 to time=15 and a customer with id=27 from time=10 to time=20. So the average time is ( (15-3) + (20-10) ) / 2 = 11.00000
+undergroundSystem.checkIn(10, "Leyton", 24);
+undergroundSystem.getAverageTime("Leyton", "Waterloo");          // return 11.00000
+undergroundSystem.checkOut(10, "Waterloo", 38);
+undergroundSystem.getAverageTime("Leyton", "Waterloo");          // return 12.00000 
+ */
+
+class UndergroundSystem {
+    constructor(){
+      this.idMap = new Map();
+      this.durationMap = new Map();
+    }
+      
+  };
+  
+  /** 
+   * @param {number} id 
+   * @param {string} stationName 
+   * @param {number} t
+   * @return {void}
+   */
+  UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+      this.idMap.set(id, {stationName, checkInTime: t})
+  };
+  
+  /** 
+   * @param {number} id 
+   * @param {string} stationName 
+   * @param {number} t
+   * @return {void}
+   */
+  UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
+      const startStation = this.idMap.get(id)
+      if(!startStation){
+        throw new Error(`Customer ${id} did not check in.`)
+      }
+      const trip = `${startStation.stationName}, ${stationName}`
+      const duration = t - startStation.checkInTime
+      if(this.durationMap.has(trip)){
+        const selectedTrip = this.durationMap.get(trip)
+        this.durationMap.set(trip, {totalTime: selectedTrip.totalTime + duration, count: selectedTrip.count + 1})
+      } else {
+        this.durationMap.set(trip, {totalTime: duration, count: 1})
+      }
+      this.idMap.delete(id)
+  };
+  
+  /** 
+   * @param {string} startStation 
+   * @param {string} endStation
+   * @return {number}
+   */
+  UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
+      const trip = `${startStation}, ${endStation}`
+      if(this.durationMap.has(trip)){
+        const route = this.durationMap.get(trip)
+        return route.totalTime / route.count
+      } else {
+        throw new Error(`Route for ${trip} has not been recorded.`)
+      }
+  };
+  
+  /** 
+   * Your UndergroundSystem object will be instantiated and called as such:
+   * var obj = new UndergroundSystem()
+   * obj.checkIn(id,stationName,t)
+   * obj.checkOut(id,stationName,t)
+   * var param_3 = obj.getAverageTime(startStation,endStation)
+   */
